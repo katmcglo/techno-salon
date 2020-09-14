2d2f6c56b27c3a4a0c96b3c8e64b2fa715073ab3
katmcglo <cathaumann@gmail.com>
2020-09-14 15:08:48 +0200

    rename component folders to lowercase

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import BackgroundImage from "gatsby-background-image"
-import classes from "../components/BackgroundImage/background-image.module.scss"
+import classes from "../components/backgroundimage/background-image.module.scss"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BookButton from "../components/bookbutton/bookbutton"
