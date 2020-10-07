3f44d64e02fd0e75af6c169e9e9dbf0e0b428382
katmcglo <cathaumann@gmail.com>
2020-10-07 20:06:05 +0200

    added masonry grid

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,13 +1,69 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import Masonry from 'react-masonry-css'
+import classes from '../styles/gallery.module.scss';
 
-const GalleryPage = () => (
+const GalleryPage = () => {
+  const data = useStaticQuery(graphql`
+    query Images {
+      images: allFile(filter: { relativeDirectory: { eq: 
+        "progallery" } }) {
+          nodes {
+            id
+              childImageSharp {
+                fixed {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+          }
+        }
+      image: file(relativePath: {eq: "progallery/pexels-1.jpg"}) {
+        id
+        childImageSharp {
+          fixed(
+            width: 400
+            ) {
+            ...GatsbyImageSharpFixed
+          }
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      } 
+    }
+  `)
+
+  const breakpointColumnsObj = {
+    default: 3,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
+
+  console.log(data);
+
+  return(
   <Layout>
     <SEO title="Image Gallery" />
-    <h1>Hi from the gallery page</h1>
-    <p>Welcome to page 2</p>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className={classes.MyMasonryGrid}
+        columnClassName={classes.MyMasonryGridColumn}>
+        {data.images.nodes.map(image => (
+          <Img key={image.childImageSharp.id} fixed={image.childImageSharp.fixed} />
+        ))}
+      </Masonry>
+    {/* <div>
+      {data.images.nodes.map(image => (
+        <Img key={image.childImageSharp.id} fixed={image.childImageSharp.fixed} />
+      ))}
+    </div> */}
   </Layout>
-)
+  )
+}
+
 
 export default GalleryPage
