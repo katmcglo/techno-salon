b9e51a382c08bd2c87982177cc80ccda2c08b474
katmcglo <cathaumann@gmail.com>
2020-10-13 18:44:24 +0200

    added background image to form page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,15 +7,18 @@ import Img from 'gatsby-image';
 const AboutPage = (props) => (
   <Layout>
     <SEO title="About" />
-    <div className={classes.Header}>
-      <h2>About Our Team</h2>
-    </div>
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "90vh"}}>
-      <div style={{width: "30rem", height: "80%", maxHeight: "50rem", color: "black", display: "flex", alignItems: "stretch", padding: "2rem"}}>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    <div style={{ height: "80vh"}}>
+      <div className={classes.Header}>
+        <h3>About Our Team</h3>
       </div>
-      <Img fluid={props.data.image.childImageSharp.fluid} style={{width: "40%", height: "80%", maxHeight: "60rem"}}></Img>
-    </div>
+      <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div style={{ width: "30rem", height: "80%", maxHeight: "50rem", color: "black", display: "flex", alignItems: "stretch", padding: "2rem" }}>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </div>
+        <Img fluid={props.data.image.childImageSharp.fluid} style={{ width: "40%", height: "80%", maxHeight: "60rem" }}></Img>
+      </div>
+      </div>
+      
     
   </Layout>
 )
