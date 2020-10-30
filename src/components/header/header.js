bf9ee45a09ff30d6a3c611e1870b5ceb06626e6d
katmcglo <cathaumann@gmail.com>
2020-10-30 16:53:42 +0100

    added window qualifier to fix deployment issue

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,41 +1,41 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect } from "react"
 import classes from "./header.module.scss"
 import BookButton from '../bookbutton/bookbutton'
 import NavigationDropdownButton from '../navigationdropdownbutton/navigationdropdownbutton'
 
 const Header = ({ siteTitle, click }) => {
-
-
-  if (window.location.pathname === "/") {
-    return null;
-  } else {
-  return(
-  <header
-    style={{
-      background: `none`
-    }}
-  >
-    <div
-      className={classes.Header}
-      
-    >
-      <h3 className={classes.HeaderText}>
-        <Link
-          to="/"
-          className={classes.HomeLink}
+  if (typeof window !== `undefined`) {
+    if (window.location.pathname === "/") {
+      return null;
+    } else {
+      return (
+        <header
+          style={{
+            background: `none`
+          }}
         >
-          {siteTitle}
-        </Link>
-      </h3>
-      <div style={{display: `flex`, alignItems: `center`, height: `100%`, padding: `0 1rem`}}>
-        <BookButton />
-        <NavigationDropdownButton click={click} />
-      </div>
-    </div>
-  </header>
-  )
+          <div
+            className={classes.Header}
+
+          >
+            <h3 className={classes.HeaderText}>
+              <Link
+                to="/"
+                className={classes.HomeLink}
+              >
+                {siteTitle}
+              </Link>
+            </h3>
+            <div style={{ display: `flex`, alignItems: `center`, height: `100%`, padding: `0 1rem` }}>
+              <BookButton />
+              <NavigationDropdownButton click={click} />
+            </div>
+          </div>
+        </header>
+      )
+    }
   }
 }
 
