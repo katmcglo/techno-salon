2643ee00cdc2570de9a0c2b675bd8c368a1c2dbe
katmcglo <cathaumann@gmail.com>
2020-10-30 19:08:28 +0100

    added site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Cutting Shapes`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `An example hairdresser's website built in React/Gatsby.js`,
     author: `@gatsbyjs`,
   },
   plugins: [
